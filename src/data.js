25e7f0c774c120fa61ff8ff438e21be16399155f
Carolina Morales <carovms@gmail.com>
2021-03-04 09:03:24 -0300

    funcion para recorrer data de campeones

--- src/data.js
+++ src/data.js
@@ -1,12 +1,15 @@
 // Hacer funcion obtenerCampeones 
 export const obtenerCampeones = (lol, tag) => {
-
-  console.log(lol);
-
-  for (champion in lol) {
-    champion.tags
+let champFilter = [];
+  for (let champion in lol) {
+    let tags = lol[champion].tags;
+    for (let i = 0; i < tags.length; i++) {
+      if (tags[i] == tag){
+        champFilter.push(lol[champion]);
+      }
+    }
   }
-
+  console.log(champFilter);
 
   return 'example';
 };
